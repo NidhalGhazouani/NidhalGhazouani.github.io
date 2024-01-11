9b0662b4e4cd62521b0a75490bb27cf46229a91f
Nidhal Ghazouani <86621140+NidhalGhazouani@users.noreply.github.com>
2024-01-11 19:49:14 +0100

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["DevOps engineering", "System engineering", "Web development"],
+    strings: ["DevOps engineering", "System engineering", "Web development", "Cloud engineering"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
