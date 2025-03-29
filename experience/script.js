30c53eb021ca6a1ca94b2fba5cf79eedba7c81e7
NidhalGhazouani <nidhal.ghazouani@etudiant-isi.utm.tn>
2025-03-29 23:13:14 +0100

    Update certifcate

--- experience/script.js
+++ experience/script.js
@@ -80,7 +80,7 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
+        document.title = "Experience | Portfolio Nidhal Ghazouani";
         $("#favicon").attr("href","/assets/images/favicon.png");
     }
     else {
